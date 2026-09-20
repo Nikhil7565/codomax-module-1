531679725b69dc2e025c025d75c969d347922fbf
Nikhil Agrawal <agrawalnikhil039@gmail.com>
2026-09-20 15:17:12 +0530

    Add variables and print statements in variables.py

---
+++ Day-3-Python/variables.py
@@ -0,0 +1,11 @@
+# Day 3 - Python Variables
+
+name = "Nikhil"
+age = 20
+height = 5.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)

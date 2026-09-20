0f360bc8ed8a00aa5ba865d91d5e55e739dcb166
Nikhil Agrawal <agrawalnikhil039@gmail.com>
2026-09-20 15:25:46 +0530

    Add grade calculator based on user marks

---
+++ Day-4-Python-Practice/grade_calculator.py
@@ -0,0 +1,16 @@
+# Day 4 - Grade Calculator
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade:", grade)

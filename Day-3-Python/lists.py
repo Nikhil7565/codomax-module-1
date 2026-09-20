45b260adcf3a14df53688e0e29ed6ed6c3344ba6
Nikhil Agrawal <agrawalnikhil039@gmail.com>
2026-09-20 15:19:38 +0530

    Create lists.py

---
+++ Day-3-Python/lists.py
@@ -0,0 +1,16 @@
+# Day 3 - Lists
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Fruits:", fruits)
+print("First fruit:", fruits[0])
+print("Number of fruits:", len(fruits))
+
+# Add an item
+fruits.append("Grapes")
+
+print("After adding Grapes:", fruits)
+
+print("\nAll fruits:")
+for fruit in fruits:
+    print(fruit)

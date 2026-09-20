43748cafab9f29f9d7a045fedb685bfcedcc0293
Nikhil Agrawal <agrawalnikhil039@gmail.com>
2026-09-20 15:25:07 +0530

    Add factorial calculation script in Python

---
+++ Day-4-Python-Practice/factorial.py
@@ -0,0 +1,13 @@
+# Day 4 - Factorial
+
+number = int(input("Enter a number: "))
+
+factorial = 1
+
+if number < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    for i in range(1, number + 1):
+        factorial *= i
+
+    print("Factorial:", factorial)
